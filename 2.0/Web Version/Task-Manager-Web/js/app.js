48afcd1146a0cbb977b95e59e18b5cdccc274cbd
Nasir Ayofe <ayofe.nasir@gmail.com>
2016-12-25 08:12:25 -0500

    Delete app.js

--- 2.0/Web Version/Task-Manager-Web/js/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('newapp', []);
-app.controller('newCtrl', function ($scope, $http) {
-    
-            $scope.sendPost = function() {
-            var data = $scope.note+";"+$window.index;
-                console.log(data);
-            $http.post("http://localhost/Task-Manager/index.php/Receiveapi_controller/note/",data);
-    };
-});
